a32d51d6648161d0e32a0ae73774bcbb8b089706
Mark Haskamp <mark.haskamp@pinnsg.com>
2010-01-17 12:29:56 -0500

    initial load

---
+++ init.el
@@ -0,0 +1,14 @@
+
+(global-hl-line-mode 1)  ;; highlight current line
+(setq standard-indent 2)
+(setq-default indent-tabs-mode nil)  ;; use spaces insted of tabs
+(setq make-backup-files nil)         ;; don't create the backup ~ files
+
+(line-number-mode 1)  ;; doesn't work on windows :-(
+
+;; ========== set colors ==========
+;;(set-cursor-color "red")
+;;(set-mouse-color "goldenrod")
+(set-face-background 'region "blue")
+;;(set-background-color "black")
+
