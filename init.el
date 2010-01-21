6cbe1946323b8c79a7bff5735b950de5ab31bd94
Mark Haskamp <mark.haskamp@pinnsg.com>
2010-01-21 16:13:49 -0500

    yasnippets

--- init.el
+++ init.el
@@ -1,4 +1,5 @@
 (add-to-list 'load-path "~/.emacs.d/color-theme-6.6.0")
+(add-to-list 'load-path "~/.emacs.d/plugins/yasnippet-0.6.1c")
 
 ;; (global-hl-line-mode 1)  ;; highlight current line
 (setq standard-indent 2)
@@ -10,3 +11,8 @@
 	'(progn
 		(color-theme-initialize)
 		(color-theme-arjen)))
+
+(require 'yasnippet)
+(yas/initialize)
+(yas/load-directory "~/.emacs.d/plugins/yasnippet-0.6.1c/snippets")
+
