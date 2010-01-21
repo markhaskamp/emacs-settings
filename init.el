1f288581b883335d4d77bf73e00fb8a918662fb6
Mark Haskamp <mark.haskamp@pinnsg.com>
2010-01-21 11:10:05 -0500

    color-theme

--- init.el
+++ init.el
@@ -1,14 +1,12 @@
+(add-to-list 'load-path "~/.emacs.d/color-theme-6.6.0")
 
-(global-hl-line-mode 1)  ;; highlight current line
+;; (global-hl-line-mode 1)  ;; highlight current line
 (setq standard-indent 2)
 (setq-default indent-tabs-mode nil)  ;; use spaces insted of tabs
 (setq make-backup-files nil)         ;; don't create the backup ~ files
 
-(line-number-mode 1)  ;; doesn't work on windows :-(
-
-;; ========== set colors ==========
-;;(set-cursor-color "red")
-;;(set-mouse-color "goldenrod")
-(set-face-background 'region "blue")
-;;(set-background-color "black")
-
+(require 'color-theme)
+(eval-after-load "color-theme"
+	'(progn
+		(color-theme-initialize)
+		(color-theme-arjen)))
